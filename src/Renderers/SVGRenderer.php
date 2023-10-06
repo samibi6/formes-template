f30c6b2b9ac6c0e860f6ecdfaac4fbd4fc6fd9e7
samibi6 <sambiname@yahoo.fr>
2023-10-06 17:07:15 +0200

    svg methods

--- src/Renderers/SVGRenderer.php
+++ src/Renderers/SVGRenderer.php
@@ -2,4 +2,7 @@
 
 namespace Opmvpc\Formes\Renderers;
 
-class SVGRenderer implements Renderer {}
+class SVGRenderer implements Renderer {
+    public function render(): string {}
+    public function save(string $path): void {}
+}
